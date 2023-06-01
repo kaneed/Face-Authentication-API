4fb56b960d123fadbe0668fab88af96ef9711e76
Heklo1 <74341161+kaneed@users.noreply.github.com>
2023-06-01 16:28:48 -0400

    Improved Sample Client
    - Added 3 sample images to test against the API
    
    - Added another case to the sample client of an imposter authentication attempt.

--- sample_client.py
+++ sample_client.py
@@ -2,8 +2,9 @@ import requests
 import json
 import os
 
+
 ## ENROLL ##
-print("Enrollment")
+print("Enrollment of Al_Gore_0001")
 url = "http://127.0.0.1:5000/enroll"
 file = 'Al_Gore_0001.jpg'
 
@@ -18,10 +19,29 @@ r = requests.post(url=url, files=files)
 response = json.loads(r.content)
 print('Error: "{}"'.format(response['error']))
 
-## AUTHENTICATE ##
-print("\n\n\nAuthentication")
+
+## GENUINE AUTHENTICATION ##
+print("\n\n\nAuthentication of Al_Gore_0002")
 url = "http://127.0.0.1:5000/authenticate"
 file = 'Al_Gore_0002.jpg'
+
+payload = { 'identifier': 'Al_Gore' }
+
+files = {
+    'json': (None, json.dumps(payload), 'application/json'),
+    'image': (os.path.basename(file), open(file, 'rb'), 'application/octet-stream')
+}
+
+r = requests.post(url=url, files=files)
+response = json.loads(r.content)
+print('Error: "{}"'.format(response['error']))
+print('Match: "{}"'.format(response['match']))
+
+
+## IMPOSTER AUTHENTICATION ##
+print("\n\n\nAuthentication of Abdullah_Gul_0001")
+url = "http://127.0.0.1:5000/authenticate"
+file = 'Abdullah_Gul_0001.jpg'
 
 payload = { 'identifier': 'Al_Gore' }
 
