4b409a895dd09f8d977d7a42799db4318f8f3d75
Heklo1 <74341161+kaneed@users.noreply.github.com>
2023-06-01 23:08:30 -0400

    Clean up sample client
    sample_client.py: Pushed enroll and authenticate into functions to remove redundant code and have a cleaner example.

--- sample_client.py
+++ sample_client.py
@@ -2,35 +2,44 @@ import requests
 import json
 import os
 
-## ENROLL ##
-print("Enrollment")
-url = "http://127.0.0.1:5000/enroll"
-file = 'Al_Gore_0001.jpg'
 
-payload = { 'identifier': 'Al_Gore' }
+def enroll(user, file):
+	print("Enrollment of " + user + " with file " + file)
+	url = "http://127.0.0.1:5000/enroll"
 
-files = {
-    'json': (None, json.dumps(payload), 'application/json'),
-    'image': (os.path.basename(file), open(file, 'rb'), 'application/octet-stream')
-}
+	payload = { 'identifier': user }
 
-r = requests.post(url=url, files=files)
-response = json.loads(r.content)
-print('Error: "{}"'.format(response['error']))
+	files = {
+		'json': (None, json.dumps(payload), 'application/json'),
+		'image': (os.path.basename(file), open(file, 'rb'), 'application/octet-stream')
+	}
+	r = requests.post(url=url, files=files)
+	response = json.loads(r.content)
+	print('Error: "{}"'.format(response['error']))
+	print("\n\n")
 
-## AUTHENTICATE ##
-print("\n\n\nAuthentication")
-url = "http://127.0.0.1:5000/authenticate"
-file = 'Al_Gore_0002.jpg'
 
-payload = { 'identifier': 'Al_Gore' }
+def authenticate(user, file):
+	print("Authentication of " + user + " with file " + file)
+	url = "http://127.0.0.1:5000/authenticate"
 
-files = {
-    'json': (None, json.dumps(payload), 'application/json'),
-    'image': (os.path.basename(file), open(file, 'rb'), 'application/octet-stream')
-}
+	payload = { 'identifier': user }
 
-r = requests.post(url=url, files=files)
-response = json.loads(r.content)
-print('Error: "{}"'.format(response['error']))
-print('Match: "{}"'.format(response['match']))
+	files = {
+		'json': (None, json.dumps(payload), 'application/json'),
+		'image': (os.path.basename(file), open(file, 'rb'), 'application/octet-stream')
+	}
+
+	r = requests.post(url=url, files=files)
+	response = json.loads(r.content)
+	print('Error: "{}"'.format(response['error']))
+	print('Match: "{}"'.format(response['match']))
+	print("\n\n")
+
+
+# Enroll user Al_Gore.
+enroll("Al_Gore", "Al_Gore_0001.jpg") 
+
+# Authenticate user Al_Gore with a genuine image and an imposter image.
+authenticate("Al_Gore", "Al_Gore_0002.jpg") # Genuine
+authenticate("Al_Gore", "Abdullah_Gul_0001.jpg") # Imposter
